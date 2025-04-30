7030a9823ae29d2c15b56405e64c8bddf06f7f7f
pareshsutariya <paresh.sutariya.it@gmail.com>
2025-04-30 12:27:33 -0400

    GradesController

---
+++ WebAPI/Controllers/GradesController.cs
@@ -0,0 +1,25 @@
+﻿namespace AdhiveshanGradingAPI.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GradesController : ControllerBase
+{
+    private readonly IGradesService _service;
+
+    public GradesController(IGradesService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("GetForParticipantAndProctor/{misId}/{skillCategory}/{proctorUserId}")]
+    public async Task<List<GradeModel>> GetForParticipantAndProctor(int misId, string skillCategory, int proctorUserId)
+        => await _service.GetForParticipantAndProctor(misId, skillCategory, proctorUserId);
+
+    [HttpPost]
+    public ActionResult AddOrUpdateForParticipantAndProctor(GradeUpdateModel model)
+    {
+        _service.AddOrUpdateForParticipantAndProctor(model);
+
+        return NoContent();
+    }
+}
