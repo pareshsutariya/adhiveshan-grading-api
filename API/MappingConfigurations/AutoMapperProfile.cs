9b49cc40f0a50bc2517a77df167431c4254e7fd7
pareshsutariya <paresh.sutariya.it@gmail.com>
2025-05-31 17:24:50 -0400

    refactor

--- API/MappingConfigurations/AutoMapperProfile.cs
+++ API/MappingConfigurations/AutoMapperProfile.cs
@@ -5,27 +5,27 @@ public class AutoMapperProfile : Profile
     public AutoMapperProfile()
     {
         // Adhiveshan
-        CreateMap<Entities.SkillCategory, SkillCategoryModel>().ReverseMap();
+        _ = CreateMap<SkillCategory, SkillCategoryModel>().ReverseMap();
 
-        CreateMap<Entities.User, UserModel>().ReverseMap();
-        CreateMap<Entities.User, UserCreateModel>().ReverseMap();
-        CreateMap<Entities.User, UserUpdateModel>().ReverseMap();
-        CreateMap<Entities.User, AdhiveshanPortalUserModel>().ReverseMap();
+        _ = CreateMap<User, UserModel>().ReverseMap();
+        _ = CreateMap<User, UserCreateModel>().ReverseMap();
+        _ = CreateMap<User, UserUpdateModel>().ReverseMap();
+        _ = CreateMap<User, AdhiveshanPortalUserModel>().ReverseMap();
 
-        CreateMap<Entities.GradingCriteria, GradingCriteriaModel>().ReverseMap();
-        CreateMap<Entities.GradingCriteria, GradingCriteriaCreateModel>().ReverseMap();
-        CreateMap<Entities.GradingCriteria, GradingCriteriaUpdateModel>().ReverseMap();
+        _ = CreateMap<GradingCriteria, GradingCriteriaModel>().ReverseMap();
+        _ = CreateMap<GradingCriteria, GradingCriteriaCreateModel>().ReverseMap();
+        _ = CreateMap<GradingCriteria, GradingCriteriaUpdateModel>().ReverseMap();
 
-        CreateMap<Entities.Grade, GradeModel>().ReverseMap();
-        CreateMap<Entities.Grade, GradeUpdateModel>().ReverseMap();
+        _ = CreateMap<Grade, GradeModel>().ReverseMap();
+        _ = CreateMap<Grade, GradeUpdateModel>().ReverseMap();
 
-        CreateMap<Entities.CompetitionEvent, CompetitionEventModel>().ReverseMap();
-        CreateMap<Entities.CompetitionEvent, CompetitionEventCreateModel>().ReverseMap();
-        CreateMap<Entities.CompetitionEvent, CompetitionEventUpdateModel>().ReverseMap();
+        _ = CreateMap<CompetitionEvent, CompetitionEventModel>().ReverseMap();
+        _ = CreateMap<CompetitionEvent, CompetitionEventCreateModel>().ReverseMap();
+        _ = CreateMap<CompetitionEvent, CompetitionEventUpdateModel>().ReverseMap();
 
-        CreateMap<Entities.Participant, ParticipantModel>().ReverseMap();
-        CreateMap<Entities.Configuration, ConfigurationModel>().ReverseMap();
-        CreateMap<Entities.Configuration, ConfigurationCreateModel>().ReverseMap();
-        CreateMap<Entities.Configuration, ConfigurationUpdateModel>().ReverseMap();
+        _ = CreateMap<Participant, ParticipantModel>().ReverseMap();
+        _ = CreateMap<Configuration, ConfigurationModel>().ReverseMap();
+        _ = CreateMap<Configuration, ConfigurationCreateModel>().ReverseMap();
+        _ = CreateMap<Configuration, ConfigurationUpdateModel>().ReverseMap();
     }
 }
